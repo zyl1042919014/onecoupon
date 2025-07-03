575bfd4bc6261efabc7276e9181864fc0d548ab1
zyl1042919014 <1042919014@qq.com>
2025-07-03 18:42:07 +0800

    线程池和延时队列优化接口响应时间

--- merchant-admin/src/main/java/com/nageoffer/onecoupon/merchant/admin/service/impl/CouponTaskServiceImpl.java
+++ merchant-admin/src/main/java/com/nageoffer/onecoupon/merchant/admin/service/impl/CouponTaskServiceImpl.java
@@ -3,6 +3,7 @@ package com.nageoffer.onecoupon.merchant.admin.service.impl;
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.util.IdUtil;
 import com.alibaba.excel.EasyExcel;
+import com.alibaba.fastjson2.JSONObject;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.onecoupon.framework.exception.ClientException;
 import com.nageoffer.onecoupon.merchant.admin.common.context.UserContext;
@@ -15,15 +16,18 @@ import com.nageoffer.onecoupon.merchant.admin.dto.resp.CouponTemplateQueryRespDT
 import com.nageoffer.onecoupon.merchant.admin.service.CouponTaskService;
 import com.nageoffer.onecoupon.merchant.admin.service.CouponTemplateService;
 import com.nageoffer.onecoupon.merchant.admin.service.handler.excel.RowCountListener;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RBlockingDeque;
+import org.redisson.api.RDelayedQueue;
+import org.redisson.api.RedissonClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 @Slf4j
 @Service
@@ -31,6 +35,7 @@ import java.util.concurrent.TimeUnit;
 public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponTaskDO> implements CouponTaskService {
     private final CouponTaskMapper couponTaskMapper;
     private final CouponTemplateService couponTemplateService;
+    private final RedissonClient redissonClient;
 
     private final ExecutorService executorService = new ThreadPoolExecutor(
             Runtime.getRuntime().availableProcessors(),
@@ -41,6 +46,7 @@ public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponT
             new ThreadPoolExecutor.DiscardPolicy()
     );
 
+    @Transactional(rollbackFor = Exception.class)
     @Override
     public void createCouponTask(CouponTaskCreateReqDTO requestParam) {
         CouponTemplateQueryRespDTO couponTemplate = couponTemplateService.findCouponTemplateById(requestParam.getCouponTemplateId());
@@ -60,16 +66,76 @@ public class CouponTaskServiceImpl extends ServiceImpl<CouponTaskMapper, CouponT
                         : CouponTaskStatusEnum.PENDING.getStatus()
         );
 
-        // 通过 EasyExcel 监听器获取 Excel 中所有行数
-        RowCountListener listener = new RowCountListener();
-        EasyExcel.read(requestParam.getFileAddress(), listener).sheet().doRead();
-
-        // 为什么需要统计行数？因为发送后需要比对所有优惠券是否都已发放到用户账号
-        int totalRows = listener.getRowCount();
-        couponTaskDO.setSendNum(totalRows);
-
         // 保存优惠券推送任务记录到数据库
         couponTaskMapper.insert(couponTaskDO);
 
+
+
+        JSONObject delayJsonObject = JSONObject
+                .of("fileAddress", requestParam.getFileAddress(), "couponTaskId", couponTaskDO.getId());
+        executorService.execute(() -> refreshCouponTaskSendNum(delayJsonObject));
+
+        RBlockingDeque<Object> blockingDeque = redissonClient.getBlockingDeque("COUPON_TASK_SEND_NUM_DELAY_QUEUE");
+        RDelayedQueue<Object> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
+        // 这里延迟时间设置 20 秒，原因是笃定上面线程池 20 秒之内就能结束任务
+        delayedQueue.offer(delayJsonObject, 20, TimeUnit.SECONDS);
+
+
+
+    }
+    private void refreshCouponTaskSendNum(JSONObject delayJsonObject) {
+        // 通过 EasyExcel 监听器获取 Excel 中所有行数
+        RowCountListener listener = new RowCountListener();
+        EasyExcel.read(delayJsonObject.getString("fileAddress"), listener).sheet().doRead();
+        int totalRows = listener.getRowCount();
+
+        // 刷新优惠券推送记录中发送行数
+        CouponTaskDO updateCouponTaskDO = CouponTaskDO.builder()
+                .id(delayJsonObject.getLong("couponTaskId"))
+                .sendNum(totalRows)
+                .build();
+        couponTaskMapper.updateById(updateCouponTaskDO);
+    }
+
+    @PostConstruct
+    public void init() {
+        new RefreshCouponTaskDelayQueueRunner(couponTaskMapper, redissonClient, this).run();
+    }
+
+
+    @RequiredArgsConstructor
+    static class RefreshCouponTaskDelayQueueRunner  {
+
+        private final CouponTaskMapper couponTaskMapper;
+        private final RedissonClient redissonClient;
+        private final CouponTaskServiceImpl couponTaskService;
+
+
+        public void run () {
+            Executors.newSingleThreadExecutor(
+                            runnable -> {
+                                Thread thread = new Thread(runnable);
+                                thread.setName("delay_coupon-task_send-num_consumer");
+                                thread.setDaemon(Boolean.TRUE);
+                                return thread;
+                            })
+                    .execute(() -> {
+                        RBlockingDeque<JSONObject> blockingDeque = redissonClient.getBlockingDeque("COUPON_TASK_SEND_NUM_DELAY_QUEUE");
+                        for (; ; ) {
+                            try {
+                                // 获取延迟队列已到达时间元素
+                                JSONObject delayJsonObject = blockingDeque.take();
+                                if (delayJsonObject != null) {
+                                    // 获取优惠券推送记录，查看发送条数是否已经有值，有的话代表上面线程池已经处理完成，无需再处理
+                                    CouponTaskDO couponTaskDO = couponTaskMapper.selectById(delayJsonObject.getLong("couponTaskId"));
+                                    if (couponTaskDO.getSendNum() == null) {
+                                        couponTaskService.refreshCouponTaskSendNum(delayJsonObject);
+                                    }
+                                }
+                            } catch (Throwable ignored) {
+                            }
+                        }
+                    });
+        }
     }
 }
